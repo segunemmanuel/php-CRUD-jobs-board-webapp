29e17c4fe99821a4e38ea9de86ed626ce58ecee0
segun <segunemmanuel46@gmail.com>
2021-04-18 09:17:38 -0700

    Update add_posts.php

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -1,15 +1,25 @@
 <?php
 if(isset($_POST['create_post'])){
 $post_category_id=$_POST['category'];
+$post_title=$_POST['title'];
 $post_author=$_POST['author'];
 $post_image=$_FILES['image']['name'];
-$post_image_temp=$_FILES['post_image']['tmp_name'];
+$post_image_temp=$_FILES['image']['tmp_name'];
 $post_content=$_POST['content'];
 $post_status=$_POST['status'];
 $post_tag=$_POST['tag'];
+$post_date=date('d-m-y');
+
+move_uploaded_file($post_image_temp,"./images/$post_image");
 
 
+$query="INSERT INTO posts(post_category_id,post_title,post_author,post_image,post_date,post_content,post_tags,post_status) ";
+$query.="VALUES({$post_category_id},'{$post_title}','{$post_author}','{$post_image}',now(),'{$post_content}','{$post_tag}','{$post_status}') ";
 
+$result=mysqli_query($connection,$query);
+if(!$result){
+    die("Failed".mysqli_error($connection));
+}
     
 }
 
@@ -34,7 +44,7 @@ if(!$result){
 while($row=mysqli_fetch_array($result)){
     $cat_id=$row['cat_id'];
     $cat_title=$row['cat_title'];
-    echo "<option value=''>{$cat_title}</option>";
+    echo "<option value='{$cat_id}'>{$cat_title}</option>";
 }
 ?>
 </select> 
